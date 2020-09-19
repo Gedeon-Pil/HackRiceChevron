70d88c7ca97fab1bdf3f01d7718c31552b328148
mark-pepperl <70180741+mark-pepperl@users.noreply.github.com>
2020-09-19 15:52:37 -0500

    Update read_excel.py

--- read_excel.py
+++ read_excel.py
@@ -1,6 +1,164 @@
 from openpyxl import load_workbook, Workbook
 import math
 
+def parse_date(date_str):
+
+    date_time = date_str.split()
+    if date_time[0].find(':') != -1:
+        temp = date_time[0]
+        date_time[0] = date_time[1]
+        date_time[1] = temp
+
+    date, time = date_time
+
+    if date.find('/') != -1:
+        month, day, year = date.split('/')
+    elif date.find('-') != -1:
+        year, month, day = date.split('-')
+
+    if len(day) == 1:
+        day = '0' + day
+
+    if len(month) == 1:
+        month = '0' + month
+
+    if len(year) == 2:
+        year = '20' + year
+
+    return day, month, year
+
+def distance(location1, location2):
+    return Math.sqrt((location1[0] - location2[0]) ** 2 + (location1[1] - location2[1]) ** 2)
+
+
+class Time:
+
+    def __init__(self, hour, minutes):
+
+        self.hour = hour
+        self.minutes = minutes
+
+    def add(self, other):
+        other_hour = other.get_hour()
+        other_minutes = other.get_minutes()
+        combined_minutes = self.minutes + other_minutes
+        added_hours = combined_minutes // 60 + other_hour
+        added_minutes = combined_minutes % 60
+
+        return Time(self.hour + added_hours, self.minutes + added_minutes)
+
+    def subtract(self, other):
+        if not self.greater(other):
+            return -1
+
+        other_hour = other.get_hour()
+        other_minutes = other.get_minutes()
+        difference_minutes = self.minutes - other_minutes
+        resultant_minutes = difference_minutes
+        resultant_hours = self.hour - other_hour
+
+        if difference_minutes < 0:
+            resultant_minutes += 60
+            resultant_hours -= 1
+
+        return Time(resultant_hours, resultant_minutes)
+
+
+    def greater(self, other):
+        return self.get_time() > other.get_time()
+
+    def get_time(self):
+        return self.hour, self.minutes
+
+    def get_hour(self):
+        return self.hour
+
+    def get_minutes(self):
+        return self.minutes
+
+
+class Schedule:
+
+    def __init__(self, begin_shift, end_shift):
+
+        self.begin_shift = begin_shift
+        self.end_shift = end_shift
+        self.shift_time = self.end_shift.subtract(self.begin_shift)
+        self.schedule = list()
+        self.schedule.append(list())
+        self.schedule_days = 0
+
+    def add_task(self, order, time, current_location, order_location):
+
+        if not self.available:
+            return False
+
+        if self.schedule_days == 1:
+            last_end_time = self.schedule[0][-1][2] + movement_time
+
+        else:
+            last_end_time = self.begin_shift
+
+        travel_distance = int(80 * distance(current_location, order_location))
+
+        movement_time = Time(0, travel_distance * 2)
+
+        start_time = last_end_time.add(movement_time)
+
+        remaining_time = self.end_shift.subtract(start_time)
+
+        if not time.greater(remaining_time):
+            self.schedule[0].append((order, start_time, start_time.add(time)))
+
+        else:
+            overflow_time = time.subtract(remaining_time)
+            self.schedule[0].append((order, start_time, self.end_shift))
+            day = 1
+            while overflow_time > 0:
+
+                if overflow_time.greater(self.shift_time):
+                    self.schedule[day].append((order, self.begin_shift, self.end_shift))
+                    overflow_time = overflow_time.subtract(self.shift_time)
+
+                else:
+                    self.schedule[day].append((order, self.begin_shift, self.begin_shift.add(overflow_time)))
+                    overflow_time = 0
+
+        return True
+
+
+    def is_available(self):
+        return self.schedule_days < 2
+
+    def get_time_remaining(self):
+        if not self.schedule[0]:
+            return self.shift_time
+        else:
+            return self.end_shift.subtract(self.schedule[0][-1][2])
+
+    def update(self):
+        self.schedule.pop(0)
+        if not self.schedule:
+            self.schedule.append(list())
+
+class Facility_Schedule:
+
+    def __init__(self, max_capacity):
+
+        self.schedule = list()
+        self.schedule.append(list())
+        self.max_capacity = max_capacity
+
+    def add_task(self, order, begin_time, end_time):
+
+
+
+
+
+
+
+SHIFTS = {"Morning" : (Time(4, 0), Time(14, 0)), "Evening" : (Time(14, 0), Time(23, 0))}
+
 class Read_Data():
 
     def __init__(self):
@@ -122,10 +280,14 @@ class Worker():
     def __init__(self, key, worker_fields):
 
         self.key = key
-        self.certifications = worker_fields["certifications"]
+        certifications = worker_fields["certifications"]
+        self.certifications = set(certifications.split(", "))
         self.shifts = worker_fields["shifts"]
         self.latitude = worker_fields["latitude"]
         self.longitude = worker_fields["longitude"]
+
+        self.start_shift, self.end_shift = SHIFTS[self.shifts]
+        self.schedule = Schedule(self.start_shift, self.end_shift)
 
     def get_key(self):
         return self.key
@@ -142,6 +304,9 @@ class Worker():
     def get_longitude(self):
         return self.longitude
 
+    def get_schedule(self):
+        return self.schedule
+
 
 class Facility():
 
@@ -152,17 +317,17 @@ class Facility():
         self.longitude = facility_fields["longitude"]
         self.occupancy = facility_fields["occupancy"]
 
-        def get_key(self):
-            return self.key
+    def get_key(self):
+        return self.key
 
-        def get_latitude(self):
-            return self.latitude
+    def get_latitude(self):
+        return self.latitude
 
-        def get_longitude(self):
-            return self.longitude
+    def get_longitude(self):
+        return self.longitude
 
-        def get_occupancy(self):
-            return self.occupancy
+    def get_occupancy(self):
+        return self.occupancy
 
 
 class Order():
@@ -177,26 +342,26 @@ class Order():
         self.completion_time = order_fields["completion_time"]
         self.submission_time = order_fields["submission_time"]
 
-        def get_key(self):
-            return self.key
+    def get_key(self):
+        return self.key
 
-        def get_facility(self):
-            return self.facility
+    def get_facility(self):
+        return self.facility
 
-        def get_type(self):
-            return self.type
+    def get_type(self):
+        return self.type
 
-        def get_id(self):
-            return self.id
+    def get_id(self):
+        return self.id
 
-        def get_priority(self):
-            return self.priority
+    def get_priority(self):
+        return self.priority
 
-        def get_completion_time(self):
-            return self.completion_time
+    def get_completion_time(self):
+        return self.completion_time
 
-        def get_submission_time(self):
-            return self.submission_time
+    def get_submission_time(self):
+        return self.submission_time
 
 
 
@@ -227,6 +392,29 @@ class Data():
         for key, order_fields in self.order_data.items():
             self.order_objs[key] = Order(key, order_fields)
 
+        distinct_days = set()
+        order_days = list()
+
+        for key, order in self.order_objs.items():
+
+            submission_time = order.get_submission_time()
+            order_day = int(parse_date(submission_time)[0])
+            distinct_days.add(order_day)
+            order_days.append((key, order_day))
+
+        min_day = min(distinct_days)
+        max_day = max(distinct_days)
+        total_days = max_day - min_day + 1
+
+        self.daily_order_objs = list()
+        for _ in range(total_days):
+            self.daily_order_objs.append(dict())
+        for order in order_days:
+            key, order_day = order
+            day_offset = order_day - min_day
+            self.daily_order_objs[day_offset][key] = self.get_order(key)
+
+
     def get_equipment(self, name):
         if name in self.equipment_objs:
             return self.equipment_objs[name]
@@ -247,11 +435,25 @@ class Data():
             return self.order_objs[name]
         return None
 
+    def get_all_equipment(self):
+        return self.equipment_objs
+
+    def get_all_workers(self):
+        return self.worker_objs
+
+    def get_all_facilities(self):
+        return self.facility_objs
+
+    def get_all_orders(self):
+        return self.order_objs
+
+    def get_all_daily_orders(self):
+        return self.daily_order_objs
+
 
 def main():
     data = Data()
-    bob = data.get_worker("Bob")
-    print(bob.get_shifts())
+    print(data.get_worker("Bob").get_certifications())
 
 if __name__ == '__main__':
     main()

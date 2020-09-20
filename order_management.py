d7693e463eabced82620569164c2227eadc4aacd
mark-pepperl <70180741+mark-pepperl@users.noreply.github.com>
2020-09-19 22:15:45 -0500

    Update order_management.py

--- order_management.py
+++ order_management.py
@@ -1,6 +1,7 @@
 from read_excel import *
-import heapq
 from random import randrange
+import heapq
+import math
 
 
 class Delegate_Tasks:
@@ -8,7 +9,7 @@ class Delegate_Tasks:
     def __init__(self):
 
         self.data = Data()
-        self.daily_orders = self.data.get_all_orders()
+        self.daily_orders = self.data.get_all_daily_orders()
         self.heap = list()
         self.workers = self.data.get_all_workers()
 
@@ -17,13 +18,17 @@ class Delegate_Tasks:
 
         current_day = 0
 
-        while True:
+        while current_day < len(self.daily_orders):
 
-            available_workers = {key : worker for key, worker in self.workers.items() if worker.get_schedule().is_available()]
+            print("DAY ", current_day)
+
+            available_workers = {key : worker for key, worker in self.workers.items() if worker.get_schedule().is_available()}
             new_orders = self.daily_orders[current_day]
             new_order_priorities = [(order.get_priority(), key) for key, order in new_orders.items()]
             for order in new_order_priorities:
                 heapq.heappush(self.heap, order)
+
+            print("Orders at beginning of day: %d" % len(self.heap))
 
             employee_capabilites = dict()
             for key in available_workers:
@@ -34,7 +39,7 @@ class Delegate_Tasks:
             while temp_heap:
                 qualified_workers = set()
                 priority, order_key = heapq.heappop(temp_heap)
-                order = data.get_order(order_key)
+                order = self.data.get_order(order_key)
                 order_type = order.get_type()
                 for key, worker in available_workers.items():
                     if order_type in worker.get_certifications():
@@ -43,60 +48,64 @@ class Delegate_Tasks:
 
                 order_capabilities[order_key] = qualified_workers
 
+            unclaimed_orders = list()
+
             while True:
 
                 priority, order_key = heapq.heappop(self.heap)
-                order = data.get_order(order_key)
+                order = self.data.get_order(order_key)
                 completion_time = Time(order.get_completion_time(), 0)
-                order_location = (order.get_latitude(), order.get_longitude())
 
-                facility = data.get_facility(order.get_facility())
+                facility = self.data.get_facility(order.get_facility())
                 facility_schedule = facility.get_schedule()
-                start_times = facility_scheule.get_start_times()
+                start_times = facility_schedule.get_start_times(completion_time)
 
                 qualified_workers = order_capabilities[order_key]
                 candidate = None
-                max_time_remaining = 0
-                min_max_priority_alternate = -1
+                max_time_remaining = ZERO_TIME
+                min_max_priority_alternate = float('inf')
                 for key in qualified_workers:
-                    worker = data.get_worker(key)
-                    if len(employee_capabilites[worker]) == 1:
+                    worker = self.data.get_worker(key)
+                    if len(employee_capabilites[key]) == 1:
                         time_remaining = worker.get_schedule().get_time_remaining()
-                        if time_remaining > max_time_remaining:
+                        if time_remaining.greater(max_time_remaining):
                             candidate = key
                             max_time_remaining = time_remaining
-                    else if not max_time_remaining:
+                    elif max_time_remaining.equals(ZERO_TIME):
+
                         other_tasks = employee_capabilites[key] - {(priority, order_key)}
+
                         max_priority_alternate = max(other_tasks)[0]
+
                         if max_priority_alternate < min_max_priority_alternate or min_max_priority_alternate == -1:
                             candidate = key
                             min_max_priority_alternate = max_priority_alternate
 
                 if candidate:
-                    worker = data.get_worker(candidate)
+                    worker = self.data.get_worker(candidate)
                     schedule = worker.get_schedule()
-                    next_available_time = schedule.get_next_start_time()
+                    next_available_time = schedule.get_next_start_time(worker.get_location(), facility.get_location())
 
                     under_capacity = False
                     for start_time in start_times:
-                        if start_time[0].get_time() <= next_available_time <= start_time[1].get_times():
+                        if start_time[0].get_time() <= next_available_time.get_time() <= start_time[1].get_time():
                             under_capacity = True
                             break
 
-                    if under_capacity and schedule.add_task(order_key, Time(completion_time, 0), (worker.get_latitude(), worker.get_longitude()), (order_location)):
+                    if under_capacity and schedule.add_task(order, completion_time, facility.get_location()):
 
                         if not schedule.is_available():
                             if candidate in employee_capabilites:
                                 employee_capabilites.remove(candidate)
 
                     else:
-                        heapq.heappush(self.heap, (priority, order_key))
+                        unclaimed_orders.append((priority, order_key))
 
                 else:
-                    heapq.heappush(self.heap, (priority, order_key))
+                    unclaimed_orders.append((priority, order_key))
 
-                if order_key in order_capabilities:
-                    order_capabilities.remove(order_key)
+
+                order_capabilities.pop(order_key, None)
 
                 for key, tasks in employee_capabilites.items():
                     if (priority, order_key) in tasks:
@@ -112,13 +121,29 @@ class Delegate_Tasks:
                 if no_capabilites:
                     break
 
-            # Update schedules at the end of each day
+            for order in unclaimed_orders:
+                    heapq.heappush(self.heap, order)
 
             for worker in self.workers.values():
+                print(worker.get_key())
+                schedule = worker.get_schedule()
+                schedule.print_today()
                 worker.get_schedule().update()
+                print('')
 
             current_day += 1
 
+            print("Orders at end of day: %d" % len(self.heap))
 
-            def daily_schedule(self, greedy):
-                pass
+        print(self.heap)
+
+
+    def daily_schedule(self, greedy):
+            pass
+
+def main():
+    framework = Delegate_Tasks()
+    framework.generate_schedule()
+
+if __name__ == '__main__':
+    main()

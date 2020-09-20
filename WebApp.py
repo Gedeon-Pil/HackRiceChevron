0ab2053157a6443f8fd3291f7bed5ab9e3b22f78
AgnaParikh <70676044+AgnaParikh@users.noreply.github.com>
2020-09-20 05:21:14 -0400

    Add files via upload

--- WebApp.py
+++ WebApp.py
@@ -1,47 +1,108 @@
+  
 import streamlit as st
 from twilio.rest import Client
 from order_management import *
+import time
 
-def runSide():
-    
-    
-    st.sidebar.markdown("<h3 style='text-align:center; color:blue;'>Schedule</h3>",unsafe_allow_html=True)
+def convert_time(hour, minutes):
 
-    st.markdown("<h1 style='text-align:center; color:blue;'>Work Oder Tracking System for Managers and Employees</h1>",unsafe_allow_html=True)
-    Worker_name=st.sidebar.text_area('Enter name of Worker: ')
-    
+    minutes = str(int(minutes))
 
-    st.write('Name of Worker is:'+Worker_name)
+    if len(minutes) == 1:
+        minutes = "0" + minutes
 
-    #Time= Python Script
-    st.write('Time')
+    hour = int(hour)
+    if hour < 12:
+        tod = "AM"
+    else:
+        hour %= 12
+        tod = "PM"
 
-    #Start_Time= PythonScript
-    st.write('Start_Time')
+    return str(hour) + ":" + minutes + tod
 
-    #To send SMS Python Script
-    text= message()
-    st.write(text)
-    
 
-def message(order_id, facility, start_time_hours, start_time_minutes, end_time_hours, end_time_minutes):
+
+def create_webpage():
+
+   st.sidebar.markdown("<h3 style='text-align:center; color:blue;'>Schedule</h3>",unsafe_allow_html=True)
+
+   st.markdown("<h1 style='text-align:center; color:blue;'>Work Order Tracking System for Managers and Employees</h1>",unsafe_allow_html=True)
+
+
+def message(name, order_id, facility, start_time_hours, start_time_minutes, end_time_hours, end_time_minutes):
     account_sid = 'AC705f999c86a965d4f4a8db2f4f7fea8b'
     auth_token = '8756cbaaba1036256d0679cbc751d300'
     client = Client(account_sid, auth_token)
+    full_message = "Hi, " + name + "! "+ "Please proceed to Facility " + facility[-1] + " to complete work order " + str(order_id) + " from " + convert_time(start_time_hours, start_time_minutes) + " to " + convert_time(end_time_hours, end_time_minutes) + "."
 
     message = client.messages \
         .create(
-            body='Proceed to '+ facility + " to complete work order " + str(order_id) + " from "+ str(start_time_hours)+ ":" + str(start_time_minutes) + " to " + str(end_time_hours) + ":" + str(end_time_hours)+".",
+            body= full_message,
             from_='+17163174817',
-            to='+17158282001'
+            to='+15086850814'
      )
 
     return(message.sid)
-    
-    
+
+
+def display_message(master_scheduler):
+
+    for val in master_scheduler.values():
+        length = len(val)
+        break
+
+    for day in range(length):
+        for name, schedules in master_scheduler.items():
+            schedule = schedules[day]
+            for order in schedule:
+                order_id = order[0]
+                facility = order[1]
+                start_time_hours = order[2]
+                start_time_minutes = order[3]
+                end_time_hours = order[4]
+                end_time_minutes =order[5]
+                message(name, order_id, facility, start_time_hours, start_time_minutes, end_time_hours, end_time_minutes)
+                time.sleep(1.5)
+        time.sleep(10)
+
+
+def display_webpage(master_scheduler):
+
+    create_webpage()
+
+    for val in master_scheduler.values():
+        length = len(val)
+        break
+
+    for day in range(length):
+        st.write(" ")
+        st.write("Day"+ str(day + 1) + ":")
+        for name, schedules in master_scheduler.items():
+            st.write("Worker Name: " + name)
+            schedule = schedules[day]
+            for order in schedule:
+                st.write()
+                order_id = order[0]
+                facility = order[1]
+                start_time_hours = order[2]
+                start_time_minutes = order[3]
+                end_time_hours = order[4]
+                end_time_minutes = order[5]
+                msg = "Order " + str(order_id) + " at facility" + str(facility[-1]) + " -- "
+                duration = convert_time(start_time_hours, start_time_minutes) + " to " + convert_time(end_time_hours, end_time_minutes)
+                st.write(msg + duration)
+
+
+
 def main():
     framework = Delegate_Tasks()
     framework.generate_schedule()
     master_scheduler = framework.get_all_schedules()
-    runSide()
-    
+
+    # display_message(master_scheduler)
+
+    display_webpage(master_scheduler)
+
+
+if __name__ == '__main__':
+    main()
